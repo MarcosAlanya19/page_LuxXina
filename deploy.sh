137a14b27ee685a4222124e2f5dbd96d879c28c3
MarcosAlanya19 <103856433+MarcosAlanya19@users.noreply.github.com>
2022-09-28 11:31:40 -0500

    Cambio de nombre

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:MarcosAlanya19/page_Show-Proyect.git main:gh-pages
+git push -f git@github.com:MarcosAlanya19/page_LuxXina.git main:gh-pages
 
 cd -
